2c6be2ae1713f43004a52ce7183b6eac0b54400c
chirenmeng <573363596@qq.com>
2019-08-27 16:29:03 +0800

    Iterator

--- src/views/archives.vue
+++ src/views/archives.vue
@@ -11,133 +11,137 @@
 
 <script>
 export default {
-	data() {
-		return {
-			postAll: [],
-			yearStep: []
-		}
-	},
-	components: {},
-	computed: {},
-	watch: {
-		$store: {
-			handler(res) {
-				let postAll = [...res.state.markdownAll]
-				postAll.map((item, index) => {
-					item.dateYear = item.date.substring(0, 4)
-					item.dateMonthday = item.date.substring(5, 10)
+  data() {
+    return {
+      postAll: [],
+      yearStep: []
+    }
+  },
+  components: {},
+  computed: {},
+  watch: {
+    $store: {
+      handler(res) {
+        let postAll = [...res.state.markdownAll]
+        postAll.map((item, index) => {
+          item.dateYear = item.date.substring(0, 4)
+          item.dateMonthday = item.date.substring(5, 10)
 
-					if (!this.yearStep.includes(item.dateYear)) {
-						this.yearStep.push(item.dateYear)
-						this.postAll.push(item)
-						this.postAll.push({
-							dateTitle: item.dateYear,
-							dateYear: item.dateYear
-						})
-					} else {
-						this.postAll.push(item)
-					}
-					return item
-				})
-			},
-			immediate: true
-		}
-	},
-	mounted() {},
-	methods: {
-		goToPost(routeName) {
-			if (!routeName) return
-			this.$router.push({
-				name: routeName
-			})
-		}
-	}
+          if (!this.yearStep.includes(item.dateYear)) {
+            this.yearStep.push(item.dateYear)
+            this.postAll.push(item)
+            this.postAll.push({
+              dateTitle: item.dateYear,
+              dateYear: item.dateYear
+            })
+          } else {
+            this.postAll.map((pItem, pIndex) => {
+              if (pItem.dateYear == pItem.dateTitle && item.dateYear == pItem.dateYear) {
+                this.postAll.splice(pIndex, 0, item)
+              }
+            })
+          }
+          return item
+        })
+      },
+      immediate: true
+    }
+  },
+  mounted() { },
+  methods: {
+    goToPost(routeName) {
+      if (!routeName) return
+      this.$router.push({
+        name: routeName
+      })
+    }
+  }
 }
 </script>
 
 <style lang='scss' scoped>
 .archives-box {
-	font-size: 1.6rem;
-	width: 50% !important;
-    padding-top: 2rem;
+  font-size: 1.6rem;
+  width: 50% !important;
+  padding-top: 2rem;
 }
 
 @media screen and (max-width: 860px) {
-	.archives-box {
-		width: 96% !important;
-	}
+  .archives-box {
+    width: 96% !important;
+  }
 }
 
 .archives-box-title {
-	font-size: 2rem;
-	line-height: 2;
-	text-align: center;
-	margin-top: 3rem;
+  font-size: 2rem;
+  line-height: 2;
+  text-align: center;
+  margin-top: 3rem;
 }
 
 .time-step {
-	display: flex;
-	flex-direction: column;
-	margin-left: 6rem;
-    margin-top: 4%;
-    padding-right: 2rem;
+  display: flex;
+  flex-direction: column;
+  margin-left: 6rem;
+  margin-top: 4%;
+  padding-right: 2rem;
 }
 
 .step-item {
-	color: #666;
-	font-size: 1.2rem;
-	position: relative;
-	padding: 2.4rem 1.4rem;
-	display: flex;
-    align-items: center;
-    
-	&[dateTitle='true']:not(:last-child) {
-		border-bottom: 1px dashed #ccc;
-    }
-    
-	.post-title {
-		font-size: 1.6rem;
-		margin-left: 1rem;
-		user-select: none;
-		cursor: pointer;
-	}
+  color: #666;
+  font-size: 1.2rem;
+  position: relative;
+  padding: 2.4rem 1.4rem;
+  display: flex;
+  align-items: center;
 
-	.date-title {
-		font-size: 3rem;
-		font-weight: bold;
-        color: $theme-light-color;
-	}
+  &[dateTitle="true"]:not(:last-child) {
+    border-bottom: 1px dashed #ccc;
+  }
+
+  .post-title {
+    font-size: 1.6rem;
+    margin-left: 1rem;
+    user-select: none;
+    cursor: pointer;
+  }
+
+  .date-title {
+    font-size: 3rem;
+    font-weight: bold;
+    color: $theme-light-color;
+  }
 }
 
 .step-item::before {
-	content: '';
-	width: 1rem;
-	height: 1rem;
-	text-align: center;
-	border-radius: 50%;
-	background: #bbb;
-	box-sizing: border-box;
-	position: absolute;
-	top: calc(50% - 1rem);
-	left: -0.6rem;
-	transform: translate(-50%, 50%);
+  content: "";
+  width: 1rem;
+  height: 1rem;
+  text-align: center;
+  border-radius: 50%;
+  background: #bbb;
+  box-sizing: border-box;
+  position: absolute;
+  top: calc(50% - 1rem);
+  left: -0.6rem;
+  transform: translate(-50%, 50%);
 }
 
 .step-item:hover {
-	&::before {
-		background: $theme-dark-color;
-	}
+  &::before {
+    background: $theme-dark-color;
+  }
 }
 
 .step-item:not(:last-child):after {
-	content: '';
-	height: 100%;
-	width: 0.4rem;
-	position: absolute;
-	top: 0.5rem;
-	left: 0;
-	transform: translate(-100%, 50%);
-	border-left: 0.4rem solid #f5f5f5;
-	z-index: -1;
+  content: "";
+  height: 100%;
+  width: 0.4rem;
+  position: absolute;
+  top: 0.5rem;
+  left: 0;
+  transform: translate(-100%, 50%);
+  border-left: 0.4rem solid #f5f5f5;
+  z-index: -1;
 }
 </style>
--- src/views/categories.vue
+++ src/views/categories.vue
@@ -12,131 +12,134 @@
 
 <script>
 export default {
-	data() {
-		return {
-			categories: [],
-			postList: [],
-			activeTitle: ''
-		}
-	},
-	components: {},
-	computed: {},
-	watch: {
-		$store: {
-			handler(res) {
-				let postAll = [...res.state.markdownAll]
-				postAll.map(item => {
-					if (!this.categories.includes(item.categories)) {
-						this.categories.push({
-							title: item.categories,
-							children: [item]
-						})
-					} else {
-						this.categories.map(cItem => {
-							if (item.categories == cItem.title) {
-								cItem.children.push(item)
-							}
-						})
-					}
-				})
-				this.getPostList(this.categories[0])
-			},
-			immediate: true
-		}
-	},
-	mounted() {},
-	methods: {
-		getPostList(res) {
-			this.activeTitle = res.title
-			this.postList = res.children
-		},
-		goToPost(routeName) {
-			if (!routeName) return
-			this.$router.push({
-				name: routeName
-			})
-		}
-	}
+  data() {
+    return {
+      categories: [],
+      categoriesTitle: [],
+      postList: [],
+      activeTitle: ''
+    }
+  },
+  components: {},
+  computed: {},
+  watch: {
+    $store: {
+      handler(res) {
+        let postAll = [...res.state.markdownAll]
+        postAll.map(item => {
+          if (!this.categoriesTitle.includes(item.categories)) {
+            this.categoriesTitle.push(item.categories)
+            this.categories.push({
+              title: item.categories,
+              children: [item]
+            })
+          } else {
+            this.categories.map(cItem => {
+              if (item.categories == cItem.title) {
+                cItem.children.push(item)
+              }
+            })
+          }
+        })
+        this.getPostList(this.categories[0])
+      },
+      immediate: true
+    }
+  },
+  mounted() { },
+  methods: {
+    getPostList(res) {
+      this.activeTitle = res.title
+      this.postList = res.children
+    },
+    goToPost(routeName) {
+      if (!routeName) return
+      this.$router.push({
+        name: routeName
+      })
+    }
+  }
 }
 </script>
 
 <style lang='scss' scoped>
 .categories-box {
-	font-size: 1.6rem;
-	width: 70% !important;
-	padding-top: 2rem;
+  font-size: 1.6rem;
+  width: 70% !important;
+  padding-top: 2rem;
 }
 
 @media screen and (max-width: 860px) {
-	.categories-box {
-		width: 96% !important;
-	}
+  .categories-box {
+    width: 96% !important;
+  }
 }
 
 .categories-box-title {
-	font-size: 2rem;
-	line-height: 2;
-	text-align: center;
-	margin: 3rem 0 5rem;
+  font-size: 2rem;
+  line-height: 2;
+  text-align: center;
+  margin: 3rem 0 5rem;
 }
 
 .category-list,
 .post-list {
-	font-weight: bold;
-	display: flex;
-	flex-direction: column;
-	padding-left: 4rem;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  padding-left: 4rem;
 }
 
 .post-list {
-	margin-top: 1rem;
-	padding-top: 2rem;
-	border-top: 0.6rem solid #f5f5f5;
+  margin-top: 1rem;
+  padding-top: 2rem;
+  border-top: 0.6rem solid #f5f5f5;
 }
 
 .post-list-item {
-	cursor: pointer;
-	user-select: none;
+  margin-bottom: 2rem;
+  cursor: pointer;
+  user-select: none;
 }
 
 .category-list-item {
-	position: relative;
-	user-select: none;
-	margin-bottom: 1.4rem;
-	&::after {
-		content: '';
-		width: 1rem;
-		height: 1rem;
-		position: absolute;
-		left: -2.2rem;
-		top: 50%;
-		transform: translateY(-50%);
-		border-radius: 50%;
-		border: 0.1rem solid #999;
-	}
+  position: relative;
+  user-select: none;
+  margin-bottom: 1.4rem;
+  &::after {
+    content: "";
+    width: 1rem;
+    height: 1rem;
+    position: absolute;
+    left: -2.2rem;
+    top: 50%;
+    transform: translateY(-50%);
+    border-radius: 50%;
+    border: 0.1rem solid #999;
+  }
 
-	&:hover::after {
-		border: 0.1rem solid $theme-light-color;
-	}
+  &:hover::after {
+    border: 0.1rem solid $theme-light-color;
+  }
 
-	> span:first-child {
-		background-color: transparent;
-		color: $theme-light-color;
-		text-decoration: none;
-		outline: none;
-		cursor: pointer;
-	}
+  > span:first-child {
+    background-color: transparent;
+    color: $theme-light-color;
+    text-decoration: none;
+    outline: none;
+    cursor: pointer;
+  }
 
-	> span:last-child {
-		color: #bbb;
-		margin-left: 0.5rem;
-	}
+  > span:last-child {
+    color: #bbb;
+    margin-left: 0.5rem;
+  }
 }
 
 .active-title {
-	&::after {
-		border: 0.1rem solid $theme-light-color;
-		background-color: $theme-light-color;
-	}
+  &::after {
+    border: 0.1rem solid $theme-light-color;
+    background-color: $theme-light-color;
+  }
 }
 </style>
